25da1c48a199773f1e5a0ca5ce2d3d6b4f44f494
Shawn Craver <shawn@shauber.net>
2018-08-25 13:57:02 -0400

    add script to run the image

---
+++ docker-run-nvr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker run \
+        --name unifi-video \
+        --cap-add SYS_ADMIN \
+        --cap-add DAC_READ_SEARCH \
+        -p 1935:1935 \
+        -p 6666:6666 \
+        -p 7080:7080 \
+        -p 7442:7442 \
+        -p 7443:7443 \
+        -p 7444:7444 \
+        -p 7445:7445 \
+        -p 7446:7446 \
+        -p 7447:7447 \
+        -v /tmp:/var/lib/unifi-video \
+        -v /tmp:/var/lib/unifi-video/videos \
+        -e TZ=America/New_York \
+        -e PUID=99 \
+        -e PGID=100 \
+        -e DEBUG=1 \
+        shauber/unifi-video-controller:3.9.7
